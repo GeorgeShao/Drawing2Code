5d6055a17e96f70294e5f8e2a7dca38edbb140c5
George Shao <GeorgeShao123@gmail.com>
2019-05-15 22:47:19 -0400

    Added toolbar outline & dividers.

--- PyArcadePaint.py
+++ PyArcadePaint.py
@@ -1,7 +1,7 @@
 import arcade
 
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 1200
+HEIGHT = 800
 
 
 def on_update(delta_time):
@@ -11,7 +11,25 @@ def on_update(delta_time):
 def on_draw():
     arcade.start_render()
     # Draw in here...
-    arcade.draw_circle_filled(100, 100, 25, arcade.color.BLUE)
+    draw_toolbar_dividers()
+    draw_toolbar_shapes()
+
+def draw_toolbar_dividers():
+    # Draw toolbar outline
+    arcade.draw_line(100, 0, 100, 800, arcade.color.BLACK)  # right border
+    arcade.draw_line(1, 1, 1, 800, arcade.color.BLACK)  # left border
+    arcade.draw_line(0, 0, 100, 0, arcade.color.BLACK)  # bottom border
+    arcade.draw_line(1, 799, 100, 799, arcade.color.BLACK)  # top border
+
+    # Draw toolbar divider
+    arcade.draw_line(50, 0, 50, 799, arcade.color.BLACK)  # middle divider
+
+    # Draw toolbar mini dividers
+    for i in range(0, 800, 50):
+        arcade.draw_line(0, i, 100, i, arcade.color.BLACK)
+
+def draw_toolbar_shapes():
+    pass
 
 def on_key_press(key, modifiers):
     pass
