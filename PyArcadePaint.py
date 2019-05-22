7d39c7d88cdca813977d02a162b3be2ef2fb5fa9
George Shao <GeorgeShao123@gmail.com>
2019-05-22 12:32:39 -0400

    Lots of changes.

--- PyArcadePaint.py
+++ PyArcadePaint.py
@@ -1,7 +1,13 @@
 import arcade
 
-WIDTH = 1200
+
+WIDTH = 900
 HEIGHT = 800
+
+chosen_color_row = ""
+chosen_color_column = ""
+chosen_shape_row = ""
+chosen_shape_column = ""
 
 
 def on_update(delta_time):
@@ -10,12 +16,15 @@ def on_update(delta_time):
 
 def on_draw():
     arcade.start_render()
-    # Draw in here...
+
     draw_toolbar_dividers()
     draw_toolbar_shapes()
+    draw_toolbar_colors()
+
 
 def draw_toolbar_dividers():
     # Draw toolbar outline
+    arcade.draw_xywh_rectangle_filled(0, 0, 100, 800, arcade.color.TROLLEY_GREY) # background
     arcade.draw_line(100, 0, 100, 800, arcade.color.BLACK)  # right border
     arcade.draw_line(1, 1, 1, 800, arcade.color.BLACK)  # left border
     arcade.draw_line(0, 0, 100, 0, arcade.color.BLACK)  # bottom border
@@ -28,21 +37,60 @@ def draw_toolbar_dividers():
     for i in range(0, 800, 50):
         arcade.draw_line(0, i, 100, i, arcade.color.BLACK)
 
+
 def draw_toolbar_shapes():
-    # Draw square
+    # Draw squares
     arcade.draw_rectangle_filled(25, 775, 25, 25, arcade.color.BLUE)
+    arcade.draw_rectangle_outline(75, 775, 25, 25, arcade.color.BLUE)
 
-    # Draw rectangle
+    # Draw rectangles
     arcade.draw_rectangle_filled(25, 725, 35, 15, arcade.color.BLUE)
+    arcade.draw_rectangle_outline(75, 725, 35, 15, arcade.color.BLUE)
 
-    # Draw circle
+    # Draw circles
     arcade.draw_circle_filled(25, 675, 13, arcade.color.BLUE)
+    arcade.draw_circle_outline(75, 675, 13, arcade.color.BLUE)
 
-    # Draw ellipse
+    # Draw ellipses
     arcade.draw_ellipse_filled(25, 625, 18, 8, arcade.color.BLUE)
+    arcade.draw_ellipse_outline(75, 625, 18, 8, arcade.color.BLUE)
 
-    # Draw triangle
+    # Draw triangles
     arcade.draw_triangle_filled(25, 590, 10, 560, 40, 560, arcade.color.BLUE)
+    arcade.draw_triangle_outline(75, 590, 60, 560, 90, 560, arcade.color.BLUE)
+
+    # Draw arc tops
+    arcade.draw_arc_filled(25, 520, 15, 15, arcade.color.BLUE, 0, 180)
+    arcade.draw_arc_outline(75, 520, 15, 15, arcade.color.BLUE, 0, 180)
+
+    # Draw arc bottoms
+    arcade.draw_arc_filled(25, 480, 15, 15, arcade.color.BLUE, 180, 360)
+    arcade.draw_arc_outline(75, 480, 15, 15, arcade.color.BLUE, 180, 360)
+
+
+def draw_toolbar_colors():
+    # First column of colors
+    arcade.draw_rectangle_filled(25, 425, 50, 50, arcade.color.RED)
+    arcade.draw_rectangle_filled(25, 375, 50, 50, arcade.color.ORANGE)
+    arcade.draw_rectangle_filled(25, 325, 50, 50, arcade.color.YELLOW)
+    arcade.draw_rectangle_filled(25, 275, 50, 50, arcade.color.GREEN)
+    arcade.draw_rectangle_filled(25, 225, 50, 50, arcade.color.BLUE)
+    arcade.draw_rectangle_filled(25, 175, 50, 50, arcade.color.PURPLE)
+    arcade.draw_rectangle_filled(25, 125, 50, 50, arcade.color.VIOLET)
+    arcade.draw_rectangle_filled(25, 75, 50, 50, arcade.color.WHITE)
+    arcade.draw_rectangle_filled(25, 25, 50, 50, arcade.color.BLACK)
+
+    # Second column of colors
+    arcade.draw_rectangle_filled(75, 425, 50, 50, arcade.color.RED_ORANGE)
+    arcade.draw_rectangle_filled(75, 375, 50, 50, arcade.color.FLUORESCENT_ORANGE)
+    arcade.draw_rectangle_filled(75, 325, 50, 50, arcade.color.FLUORESCENT_YELLOW)
+    arcade.draw_rectangle_filled(75, 275, 50, 50, arcade.color.YELLOW_GREEN)
+    arcade.draw_rectangle_filled(75, 225, 50, 50, arcade.color.AIR_SUPERIORITY_BLUE)
+    arcade.draw_rectangle_filled(75, 175, 50, 50, arcade.color.FUCHSIA_PURPLE)
+    arcade.draw_rectangle_filled(75, 125, 50, 50, arcade.color.BLUE_VIOLET)
+    arcade.draw_rectangle_filled(75, 75, 50, 50, arcade.color.WHITE_SMOKE)
+    arcade.draw_rectangle_filled(75, 25, 50, 50, arcade.color.ASH_GREY)
+
 
 def on_key_press(key, modifiers):
     pass
@@ -53,7 +101,42 @@ def on_key_release(key, modifiers):
 
 
 def on_mouse_press(x, y, button, modifiers):
-    pass
+    global chosen_color_column, chosen_shape_column, chosen_color_row, chosen_shape_row
+
+    # choosing color & shape row * column
+    if y >= 350:
+        if x <= 50:
+            for i in range(450, 850, 50):
+                if y <= i:
+                    chosen_shape_row = i
+                    chosen_shape_column = 1
+                    break
+        elif x <= 100:
+            for i in range(450, 850, 50):
+                if y <= i:
+                    chosen_shape_row = i
+                    chosen_shape_column = 2
+                    break
+        print("")
+        print("Color: " + str(chosen_color_column) + "/" + str(chosen_color_row))
+        print("Shape: " + str(chosen_shape_column) + "/" + str(chosen_shape_row))
+
+    elif y <= 350:
+        if x <= 50:
+            for i in range(0, 450, 50):
+                if y <= i:
+                    chosen_color_row = i
+                    chosen_color_column = 1
+                    break
+        elif x <= 100:
+            for i in range(0, 450, 50):
+                if y <= i:
+                    chosen_color_row = i
+                    chosen_color_column = 2
+                    break
+        print("")
+        print("Color: " + str(chosen_color_column) + "/" + str(chosen_color_row))
+        print("Shape: " + str(chosen_shape_column) + "/" + str(chosen_shape_row))
 
 
 def setup():
