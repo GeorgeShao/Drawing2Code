1668d68c45bccea56dcede42214dd7ff8aaaf0b2
George Shao <GeorgeShao123@gmail.com>
2019-05-26 11:58:53 -0400

    Added circle & color choosing functionality.

--- PyArcadePaint.py
+++ PyArcadePaint.py
@@ -5,10 +5,10 @@ WIDTH = 900
 HEIGHT = 800
 
 # Chosen color & shape row & column variables
-chosen_color_row = ""
-chosen_color_column = ""
-chosen_shape_row = ""
-chosen_shape_column = ""
+chosen_color_row = 0
+chosen_color_column = 0
+chosen_shape_row = 0
+chosen_shape_column = 0
 
 # In-the-moment drawing variables
 currently_drawing = False
@@ -20,47 +20,86 @@ end_y = 0
 # Shape object arrays
 rectangles_filled = []
 rectangles_outline = []
+circle_filled = []
+circle_outline = []
 
 
 class RectangleFilled:
-    def __init__(self, x, y, x1, y1):
+    def __init__(self, x, y, x1, y1, color):
         self.x = x
         self.y = y
         self.x1 = x1
         self.y1 = y1
+        self.color = color
 
     def draw_shape(self):
         if self.x <= self.x1:
             if self.y <= self.y1:
-                arcade.draw_lrtb_rectangle_filled(self.x, self.x1, self.y1, self.y, arcade.color.BLUE)
+                arcade.draw_lrtb_rectangle_filled(self.x, self.x1, self.y1, self.y, self.color)
             elif self.y1 < self.y:
-                arcade.draw_lrtb_rectangle_filled(self.x, self.x1, self.y, self.y1, arcade.color.BLUE)
+                arcade.draw_lrtb_rectangle_filled(self.x, self.x1, self.y, self.y1, self.color)
         elif self.x1 < self.x:
             if self.y <= self.y1:
-                arcade.draw_lrtb_rectangle_filled(self.x1, self.x, self.y1, self.y, arcade.color.BLUE)
+                arcade.draw_lrtb_rectangle_filled(self.x1, self.x, self.y1, self.y, self.color)
             elif self.y1 < self.y:
-                arcade.draw_lrtb_rectangle_filled(self.x1, self.x, self.y, self.y1, arcade.color.BLUE)
+                arcade.draw_lrtb_rectangle_filled(self.x1, self.x, self.y, self.y1, self.color)
 
 
 class RectangleOutline:
-    def __init__(self, x, y, x1, y1):
+    def __init__(self, x, y, x1, y1, color):
         self.x = x
         self.y = y
         self.x1 = x1
         self.y1 = y1
+        self.color = color
 
     def draw_shape(self):
         if self.x <= self.x1:
             if self.y <= self.y1:
-                arcade.draw_lrtb_rectangle_outline(self.x, self.x1, self.y1, self.y, arcade.color.BLUE)
+                arcade.draw_lrtb_rectangle_outline(self.x, self.x1, self.y1, self.y, self.color)
             elif self.y1 < self.y:
-                arcade.draw_lrtb_rectangle_outline(self.x, self.x1, self.y, self.y1, arcade.color.BLUE)
+                arcade.draw_lrtb_rectangle_outline(self.x, self.x1, self.y, self.y1, self.color)
         elif self.x1 < self.x:
             if self.y <= self.y1:
-                arcade.draw_lrtb_rectangle_outline(self.x1, self.x, self.y1, self.y, arcade.color.BLUE)
+                arcade.draw_lrtb_rectangle_outline(self.x1, self.x, self.y1, self.y, self.color)
             elif self.y1 < self.y:
-                arcade.draw_lrtb_rectangle_outline(self.x1, self.x, self.y, self.y1, arcade.color.BLUE)
+                arcade.draw_lrtb_rectangle_outline(self.x1, self.x, self.y, self.y1, self.color)
 
+
+class CircleFilled:
+    def __init__(self, x, y, x1, y1, color):
+        self.x = x
+        self.y = y
+        self.x1 = x1
+        self.y1 = y1
+        self.color = color
+
+    def draw_shape(self):
+        radius = 0
+        if abs(self.x1 - self.x) >= abs(self.y1 - self.y):
+            radius = self.x1 - self.x
+        elif abs(self.x1 - self.x) < abs(self.y1 - self.y):
+            radius = self.y1 - self.y
+
+        arcade.draw_circle_filled(self.x, self.y, radius, self.color)
+
+
+class CircleOutline:
+    def __init__(self, x, y, x1, y1, color):
+        self.x = x
+        self.y = y
+        self.x1 = x1
+        self.y1 = y1
+        self.color = color
+
+    def draw_shape(self):
+        radius = 0
+        if abs(self.x1 - self.x) >= abs(self.y1 - self.y):
+            radius = self.x1 - self.x
+        elif abs(self.x1 - self.x) < abs(self.y1 - self.y):
+            radius = self.y1 - self.y
+
+        arcade.draw_circle_outline(self.x, self.y, radius, self.color)
 
 def on_update(delta_time):
     pass
@@ -69,14 +108,18 @@ def on_update(delta_time):
 def on_draw():
     arcade.start_render()
 
-    draw_toolbar_dividers()
-    draw_toolbar_shapes()
-    draw_toolbar_colors()
-
     for i in range(len(rectangles_filled)):
         RectangleFilled.draw_shape(rectangles_filled[i])
     for i in range(len(rectangles_outline)):
         RectangleOutline.draw_shape(rectangles_outline[i])
+    for i in range(len(circle_filled)):
+        CircleFilled.draw_shape(circle_filled[i])
+    for i in range(len(circle_outline)):
+        CircleOutline.draw_shape(circle_outline[i])
+
+    draw_toolbar_dividers()
+    draw_toolbar_shapes()
+    draw_toolbar_colors()
 
 
 def draw_toolbar_dividers():
@@ -88,7 +131,7 @@ def draw_toolbar_dividers():
     arcade.draw_line(1, 799, 100, 799, arcade.color.BLACK)  # top border
 
     # Draw toolbar divider
-    arcade.draw_line(50, 0, 50, 799, arcade.color.BLACK)  # middle divider
+    arcade.draw_line(50, 0, 50, 750, arcade.color.BLACK)  # middle divider
 
     # Draw toolbar mini dividers
     for i in range(0, 800, 50):
@@ -96,9 +139,6 @@ def draw_toolbar_dividers():
 
 
 def draw_toolbar_shapes():
-    # Draw squares
-    arcade.draw_rectangle_filled(25, 775, 25, 25, arcade.color.BLUE)
-    arcade.draw_rectangle_outline(75, 775, 25, 25, arcade.color.BLUE)
 
     # Draw rectangles
     arcade.draw_rectangle_filled(25, 725, 35, 15, arcade.color.BLUE)
@@ -162,15 +202,15 @@ def on_mouse_press(x, y, button, modifiers):
     global currently_drawing, start_x, start_y, end_x, end_y
 
     # choosing color & shape row * column
-    if y >= 350:
+    if 350 <= y <= 750:
         if x <= 50:
-            for i in range(450, 850, 50):
+            for i in range(450, 800, 50):
                 if y <= i:
                     chosen_shape_row = i/50
                     chosen_shape_column = 1
                     break
         elif x <= 100:
-            for i in range(450, 850, 50):
+            for i in range(450, 800, 50):
                 if y <= i:
                     chosen_shape_row = i/50
                     chosen_shape_column = 2
@@ -197,31 +237,74 @@ def on_mouse_press(x, y, button, modifiers):
         currently_drawing = True
         start_x = x
         start_y = y
-        print("Mouse Clicked")
 
 
 def on_mouse_release(x, y, button, modifiers):
-    global chosen_color_column, chosen_shape_column, chosen_color_row, chosen_shape_row
+    global chosen_color_column, chosen_shape_column, chosen_color_row, chosen_shape_row, color
     global currently_drawing, start_x, start_y, end_x, end_y
-    global rectangles_filled, rectangles_outline
+    global rectangles_filled, rectangles_outline, circle_filled, circle_outline
 
     if x > 100:
         currently_drawing = False
         end_x = x
         end_y = y
+        color = None
+
+        if chosen_color_column == 1:
+            if chosen_color_row == 1:
+                color = arcade.color.BLACK
+            if chosen_color_row == 2:
+                color = arcade.color.WHITE
+            if chosen_color_row == 3:
+                color = arcade.color.VIOLET
+            if chosen_color_row == 4:
+                color = arcade.color.PURPLE
+            if chosen_color_row == 5:
+                color = arcade.color.BLUE
+            if chosen_color_row == 6:
+                color = arcade.color.GREEN
+            if chosen_color_row == 7:
+                color = arcade.color.YELLOW
+            if chosen_color_row == 8:
+                color = arcade.color.ORANGE
+            if chosen_color_row == 9:
+                color = arcade.color.RED
+        if chosen_color_column == 2:
+            if chosen_color_row == 1:
+                color = arcade.color.ASH_GREY
+            if chosen_color_row == 2:
+                color = arcade.color.WHITE_SMOKE
+            if chosen_color_row == 3:
+                color = arcade.color.BLUE_VIOLET
+            if chosen_color_row == 4:
+                color = arcade.color.FUCHSIA_PURPLE
+            if chosen_color_row == 5:
+                color = arcade.color.AIR_SUPERIORITY_BLUE
+            if chosen_color_row == 6:
+                color = arcade.color.YELLOW_GREEN
+            if chosen_color_row == 7:
+                color = arcade.color.FLUORESCENT_YELLOW
+            if chosen_color_row == 8:
+                color = arcade.color.FLUORESCENT_ORANGE
+            if chosen_color_row == 9:
+                color = arcade.color.RED_ORANGE
+
         if chosen_shape_column == 1:
-            if chosen_shape_row == 16:
-                rectangles_filled.append(RectangleFilled(start_x, start_y, end_x, end_y))
+            if chosen_shape_row == 15:
+                rectangles_filled.append(RectangleFilled(start_x, start_y, end_x, end_y, color))
+            if chosen_shape_row == 14:
+                circle_filled.append(CircleFilled(start_x, start_y, end_x, end_y, color))
         if chosen_shape_column == 2:
-            if chosen_shape_row == 16:
-                rectangles_outline.append(RectangleOutline(start_x, start_y, end_x, end_y))
-        print("Mouse Released")
+            if chosen_shape_row == 15:
+                rectangles_outline.append(RectangleOutline(start_x, start_y, end_x, end_y, color))
+            if chosen_shape_row == 14:
+                circle_outline.append(CircleOutline(start_x, start_y, end_x, end_y, color))
 
 
 def setup():
     arcade.open_window(WIDTH, HEIGHT, "PyArcadePaint")
     arcade.set_background_color(arcade.color.WHITE)
-    arcade.schedule(on_update, 1/60)
+    arcade.schedule(on_update, 1/30)
 
     # Override arcade window methods
     window = arcade.get_window()
