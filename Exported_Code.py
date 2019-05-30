7451c1df4e1d3dec3aedd8a70f179f6852d1da0f
George Shao <GeorgeShao123@gmail.com>
2019-05-30 10:16:29 -0400

    Fixed spacing.

--- Exported_Code.py
+++ Exported_Code.py
@@ -10,13 +10,7 @@ def on_update(delta_time):
  
 def on_draw(): 
     arcade.start_render() 
-    # Drawing code here 
-    arcade.draw_circle_filled(133, 694, 336, (255, 165, 0))
-    arcade.draw_circle_filled(419, 471, 86, (255, 165, 0))
-    arcade.draw_line(122, 548, 322, 347, (255, 191, 0), line_width=5)
-    arcade.draw_line(300, 556, 466, 400, (255, 83, 73), line_width=2)
-    arcade.draw_line(434, 563, 262, 280, (255, 83, 73), line_width=2)
-    arcade.draw_line(169, 242, 556, 566, (255, 83, 73), line_width=2)
+    # Exported code will show here
 
  
 def on_key_press(key, modifiers): 
