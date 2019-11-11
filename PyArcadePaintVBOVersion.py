d6ac1ec9cab5b345aee13267b0af61ac2aea1df2
George Shao <GeorgeShao123@gmail.com>
2019-11-10 19:05:01 -0500

    Continued working on improved export code function.

--- PyArcadePaintVBOVersion.py
+++ PyArcadePaintVBOVersion.py
@@ -175,7 +175,7 @@ def on_mouse_drag(x, y, dx, dy, button, modifiers):
             if chosen_shape_row == 10:
                 drawing_width = 32
             elements.append(arcade.create_line(start_x, start_y, end_x, end_y, color=get_chosen_color(), line_width=drawing_width))
-            output_text.append(f"arcade.create_line({start_x}, {start_y}, {end_x}, {end_y}, color={get_chosen_color()}, line_width={drawing_width})")
+            output_text.append(f"elements.append(arcade.create_line({start_x}, {start_y}, {end_x}, {end_y}, color={get_chosen_color()}, line_width={drawing_width}))")
             # elements.append(arcade.create_ellipse_filled(start_x, start_y, drawing_width, drawing_width, color=get_chosen_color()))
 
 def on_mouse_press(x, y, button, modifiers):
@@ -249,8 +249,10 @@ def on_mouse_release(x, y, button, modifiers):
 WIDTH = 800
 HEIGHT = 800
 elements = None
+
 def on_update(delta_time):
     pass
+
 def on_draw():
 """)
 
@@ -263,20 +265,40 @@ def on_draw():
 
 def on_key_press(key, modifiers):
     pass
+
 def on_key_release(key, modifiers):
     pass
+
+def on_mouse_drag(x, y, dx, dy, button, modifiers):
+    pass
+
 def on_mouse_press(x, y, button, modifiers):
     pass
+
+def on_mouse_release(x, y, button, modifiers):
+    pass
+
 def setup():
-    arcade.open_window(WIDTH, HEIGHT, \"My Arcade Game\")
-    arcade.set_background_color(arcade.color.WHITE) 
-    arcade.schedule(on_update, 1/60)
-    # Override arcade window methods n
+    global elements, toolbar
+
+    arcade.open_window(WIDTH, HEIGHT, "PyArcadePaint")
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.schedule(on_update, 1/1000)
+
+    # Shape shape list
+    elements = arcade.ShapeElementList()
+    elements.center_x = 0
+    elements.center_y = 0
+    elements.angle = 0
+
+    # Override arcade window methods
     window = arcade.get_window()
     window.on_draw = on_draw
     window.on_key_press = on_key_press
     window.on_key_release = on_key_release
     window.on_mouse_press = on_mouse_press
+    window.on_mouse_release = on_mouse_release
+    window.on_mouse_drag = on_mouse_drag
     arcade.run()
 if __name__ == '__main__':
     setup()
@@ -297,13 +319,13 @@ def setup():
     arcade.set_background_color(arcade.color.WHITE)
     arcade.schedule(on_update, 1/1000)
 
-    # Shape shape lists
+    # Shape shape list
     elements = arcade.ShapeElementList()
     elements.center_x = 0
     elements.center_y = 0
     elements.angle = 0
 
-    # Toolbar shape lists
+    # Toolbar shape list
     toolbar = arcade.ShapeElementList()
     toolbar.center_x = 0
     toolbar.center_y = 0
